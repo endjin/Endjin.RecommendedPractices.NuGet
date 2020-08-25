666aab832e8763f2ffbad6e74c903bd964a5a358
MikeLarah <mike.larah@endjin.com>
2020-08-25 17:45:43 +0100

    Add alias

--- src/Endjin.RecommendedPractices.Nuget.Specs/Endjin.RecommendedPractices.Nuget.Tests.ps1
+++ src/Endjin.RecommendedPractices.Nuget.Specs/Endjin.RecommendedPractices.Nuget.Tests.ps1
@@ -22,15 +22,15 @@ Remove-Item "$env:userprofile\.nuget\packages\endjin.recommendedpractices\0.0.1-
 
 Describe 'Packaging tests' {
 
-    & nuget.exe sources Add -Name "EndjinRecommendedPracticesLocal" -Source $packageOutputDir
+    nuget sources Add -Name "EndjinRecommendedPracticesLocal" -Source $packageOutputDir
 
-    & nuget.exe pack $recommendPracticesNuspec -Version "0.0.1-local" -OutputDirectory $packageOutputDir -NoDefaultExcludes
+    nuget pack $recommendPracticesNuspec -Version "0.0.1-local" -OutputDirectory $packageOutputDir -NoDefaultExcludes
 
     dotnet build $specsSln /p:Configuration=Release
     dotnet pack (Join-Path $specsSlnDir "SingleFramework/SingleFramework.csproj") --no-build --output $packageOutputDir /p:Configuration=Release /p:EndjinRepositoryUrl="https://github.com/endjin/Endjin.RecommendedPractices" /p:PackageVersion=0.0.1-local
     dotnet pack (Join-Path $specsSlnDir "MultiFramework/MultiFramework.csproj") --no-build --output $packageOutputDir /p:Configuration=Release /p:EndjinRepositoryUrl="https://github.com/endjin/Endjin.RecommendedPractices" /p:PackageVersion=0.0.1-local
 
-    & nuget.exe sources Remove -Name "EndjinRecommendedPracticesLocal"
+    nuget sources Remove -Name "EndjinRecommendedPracticesLocal"
 
     It 'should create a nupkg file for the SingleFramework project' {
         (Join-Path $packageOutputDir "SingleFramework.0.0.1-local.nupkg") | Should -Exist
