4125d1ff3eac544c99a25f87fd9965336e866af5
MikeLarah <mike.larah@endjin.com>
2020-08-25 15:42:16 +0100

    Wrap up in pester tests

---
+++ src/Endjin.RecommendedPractices.Nuget.Specs/Endjin.RecommendedPractices.Nuget.Tests.ps1
@@ -0,0 +1,41 @@
+$ErrorActionPreference = 'Stop'
+$here = Split-Path -Parent $MyInvocation.MyCommand.Path
+
+$recommendedPracticesDir = Join-Path $here "../Endjin.RecommendedPractices.Nuget"
+$recommendPracticesNuspec = Join-Path $recommendedPracticesDir "Endjin.RecommendedPractices.nuspec"
+$playgroundSln = Join-Path $here "Endjin.RecommendedPractices.Nuget.Playground.sln"
+$packageOutputDir = Join-Path $here "../packages"
+
+# Clear down anything left from previous run
+Remove-Item $packageOutputDir -Force -Recurse -ErrorAction SilentlyContinue
+@(".editorconfig","PackageIcon.png","stylecop.json","StyleCop.ruleset") | Remove-Item -Force -ErrorAction SilentlyContinue
+Remove-Item "$env:userprofile\.nuget\packages\endjin.recommendedpractices\0.0.1-local" -Force -Recurse -ErrorAction SilentlyContinue
+
+Describe 'Packaging tests' {
+
+    nuget sources Add -Name "EndjinRecommendedPracticesLocal" -Source $packageOutputDir
+
+    nuget pack $recommendPracticesNuspec -Version "0.0.1-local" -OutputDirectory $packageOutputDir -NoDefaultExcludes
+
+    dotnet build $playgroundSln /p:Configuration=Release
+    dotnet pack (Join-Path $here "SingleFramework/SingleFramework.csproj") --no-build --output $packageOutputDir /p:Configuration=Release /p:EndjinRepositoryUrl="https://github.com/endjin/Endjin.RecommendedPractices" /p:PackageVersion=0.0.1-local
+    dotnet pack (Join-Path $here "MultiFramework/MultiFramework.csproj") --no-build --output $packageOutputDir /p:Configuration=Release /p:EndjinRepositoryUrl="https://github.com/endjin/Endjin.RecommendedPractices" /p:PackageVersion=0.0.1-local
+
+    nuget sources Remove -Name "EndjinRecommendedPracticesLocal"
+
+    It 'should create a nupkg file for the SingleFramework project' {
+        (Join-Path $packageOutputDir "SingleFramework.0.0.1-local.nupkg") | Should -Exist
+    }
+
+    It 'should create a snupkg file for the SingleFramework project' {
+        (Join-Path $packageOutputDir "SingleFramework.0.0.1-local.snupkg") | Should -Exist
+    }
+
+    It 'should create a nupkg file for the MultiFramework project' {
+        (Join-Path $packageOutputDir "MultiFramework.0.0.1-local.nupkg") | Should -Exist
+    }
+
+    It 'should create a snupkg file for the MultiFramework project' {
+        (Join-Path $packageOutputDir "MultiFramework.0.0.1-local.snupkg") | Should -Exist
+    }
+}
---
+++ src/Endjin.RecommendedPractices.Nuget.Specs/run-tests.ps1
@@ -0,0 +1,26 @@
+$ErrorActionPreference = 'Stop'
+$here = Split-Path -Parent $PSCommandPath
+$pesterVer = '4.10.1'
+try {
+    [array]$existingModule = Get-Module -ListAvailable Pester
+    if (!$existingModule -or ($existingModule.Version -notcontains $pesterVer)) {
+        Install-Module Pester -RequiredVersion $pesterVer -Force -Scope CurrentUser
+    }
+    Import-Module Pester
+    $results = Invoke-Pester $here -PassThru
+
+    if ($results.FailedCount -gt 0) {
+        Write-Host ("{0} out of {1} tests failed - check previous logging for more details" -f $results.FailedCount, $results.TotalCount)
+    }
+
+    $results
+}
+catch {
+    Write-Output ("::error file={0},line={1},col={2}::{3}" -f `
+                        $_.InvocationInfo.ScriptName,
+                        $_.InvocationInfo.ScriptLineNumber,
+                        $_.InvocationInfo.OffsetInLine,
+                        $_.Exception.Message)
+
+    exit 1
+}
