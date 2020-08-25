69ce923cfc735f79c0003aee21d833bd0583b949
MikeLarah <mike.larah@endjin.com>
2020-08-25 16:28:03 +0100

    Move specs sln into folder and fix paths

--- src/Endjin.RecommendedPractices.Nuget.Specs/Endjin.RecommendedPractices.Nuget.Tests.ps1
+++ src/Endjin.RecommendedPractices.Nuget.Specs/Endjin.RecommendedPractices.Nuget.Tests.ps1
@@ -1,14 +1,23 @@
 $ErrorActionPreference = 'Stop'
 $here = Split-Path -Parent $MyInvocation.MyCommand.Path
 
-$recommendedPracticesDir = Join-Path $here "../Endjin.RecommendedPractices.Nuget"
-$recommendPracticesNuspec = Join-Path $recommendedPracticesDir "Endjin.RecommendedPractices.nuspec"
-$playgroundSln = Join-Path $here "Endjin.RecommendedPractices.Nuget.Playground.sln"
-$packageOutputDir = Join-Path $here "../packages"
+Write-Host "Here: $here"
+
+$recommendedPracticesDir = Resolve-Path (Join-Path $here "../Endjin.RecommendedPractices.Nuget")
+$recommendPracticesNuspec = Resolve-Path (Join-Path $recommendedPracticesDir "Endjin.RecommendedPractices.nuspec")
+$specsSlnDir = Resolve-Path (Join-Path $here "Solution")
+$specsSln = Resolve-Path (Join-Path $specsSlnDir "Endjin.RecommendedPractices.Nuget.Specs.sln")
+$packageOutputDir = Resolve-Path (Join-Path $here "../packages")
+
+Write-Host "Recommended Practices directory: $recommendedPracticesDir"
+Write-Host "Recommended Practices nuspec: $recommendPracticesNuspec"
+Write-Host "Specs solution directory: $specsSlnDir"
+Write-Host "Specs solution: $specsSln"
+Write-Host "Package output directory: $packageOutputDir"
 
 # Clear down anything left from previous run
 Remove-Item $packageOutputDir -Force -Recurse -ErrorAction SilentlyContinue
-@(".editorconfig","PackageIcon.png","stylecop.json","StyleCop.ruleset") | Remove-Item -Force -ErrorAction SilentlyContinue
+@(".editorconfig","PackageIcon.png","stylecop.json","StyleCop.ruleset") | ForEach-Object { Join-Path $specsSlnDir $_ } | Remove-Item -Force -ErrorAction SilentlyContinue
 Remove-Item "$env:userprofile\.nuget\packages\endjin.recommendedpractices\0.0.1-local" -Force -Recurse -ErrorAction SilentlyContinue
 
 Describe 'Packaging tests' {
@@ -17,9 +26,9 @@ Describe 'Packaging tests' {
 
     nuget pack $recommendPracticesNuspec -Version "0.0.1-local" -OutputDirectory $packageOutputDir -NoDefaultExcludes
 
-    dotnet build $playgroundSln /p:Configuration=Release
-    dotnet pack (Join-Path $here "SingleFramework/SingleFramework.csproj") --no-build --output $packageOutputDir /p:Configuration=Release /p:EndjinRepositoryUrl="https://github.com/endjin/Endjin.RecommendedPractices" /p:PackageVersion=0.0.1-local
-    dotnet pack (Join-Path $here "MultiFramework/MultiFramework.csproj") --no-build --output $packageOutputDir /p:Configuration=Release /p:EndjinRepositoryUrl="https://github.com/endjin/Endjin.RecommendedPractices" /p:PackageVersion=0.0.1-local
+    dotnet build $specsSln /p:Configuration=Release
+    dotnet pack (Join-Path $specsSlnDir "SingleFramework/SingleFramework.csproj") --no-build --output $packageOutputDir /p:Configuration=Release /p:EndjinRepositoryUrl="https://github.com/endjin/Endjin.RecommendedPractices" /p:PackageVersion=0.0.1-local
+    dotnet pack (Join-Path $specsSlnDir "MultiFramework/MultiFramework.csproj") --no-build --output $packageOutputDir /p:Configuration=Release /p:EndjinRepositoryUrl="https://github.com/endjin/Endjin.RecommendedPractices" /p:PackageVersion=0.0.1-local
 
     nuget sources Remove -Name "EndjinRecommendedPracticesLocal"
 
