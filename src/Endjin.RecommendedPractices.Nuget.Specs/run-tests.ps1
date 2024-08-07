ddc057160158cb893779f75425a5e8595c3993f3
James Dawson <james.dawson@endjin.com>
2024-08-07 15:11:37 +0100

    Skip publisher check when downgrading Pester
    This is due to the publisher certificate having been changed since v5.6.

--- src/Endjin.RecommendedPractices.Nuget.Specs/run-tests.ps1
+++ src/Endjin.RecommendedPractices.Nuget.Specs/run-tests.ps1
@@ -5,7 +5,11 @@ $pesterVer = '4.10.1'
 try {
     [array]$existingModule = Get-Module -ListAvailable Pester
     if (!$existingModule -or ($existingModule.Version -notcontains $pesterVer)) {
-        Install-Module Pester -RequiredVersion $pesterVer -Force -Scope CurrentUser
+        # Build agents now have v5.6.1 of Pester installed, which has a different publisher
+        # than earlier versions. This means that the -SkipPublisherCheck flag is required to
+        # avoid the error "not matching with the authenticode issuer" error.
+        # REF: https://github.com/pester/Pester/issues/2438
+        Install-Module Pester -RequiredVersion $pesterVer -Force -Scope CurrentUser -SkipPublisherCheck
     }
     Import-Module Pester
     $results = Invoke-Pester $here -PassThru
